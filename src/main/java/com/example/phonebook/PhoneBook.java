93912b08c28344d580ed2efcc8ce08e25ed2e791
NikolausPetrov <me@zapad23.ru>
2024-10-31 19:59:58 +0300

    add implemented

--- src/main/java/com/example/phonebook/PhoneBook.java
+++ src/main/java/com/example/phonebook/PhoneBook.java
@@ -1,7 +1,15 @@
 package com.example.phonebook;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> contacts = new HashMap<>();
+
     public int add(String name, String number) {
-        return 0;
+        if (!contacts.containsKey(name)) {
+            contacts.put(name, number);
+        }
+        return contacts.size();
     }
 }
